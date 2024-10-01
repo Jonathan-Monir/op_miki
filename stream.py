61de7da43bb767a485b69628308a6d3466fc0428
Jonathan Mamdouh Monir <2021170148@cis.asu.edu.eg>
2024-10-01 11:12:18 +0300

    last commit

--- stream.py
+++ stream.py
@@ -9,30 +9,15 @@ import re
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 # Radio button for selecting the manga version
-anime = st.radio(
-    "Choose a version",
-    ["one piece colored", "Not Colored"],
-)
 
-# Extract URLs for specific pages
-def urls_extractor(url, page):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    aa = soup.find_all('a', href=True)
-    all_decorations = [decoration for decoration in aa if 'colored' in decoration['href']]
-    page = last_chapter - (page - 1)
-    pages_url = all_decorations[page]['href']
-    
-    return pages_url
 
 # Web scraping to collect image URLs
 def web_scrape(url):
-    response = requests.get("https://mangaberri.com/" + url)
+    response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    images = soup.find_all('img', src=True)
-    image_urls = [image['src'] for image in images if 'One Piece Colored Manga' in image['alt']]
+    images = soup.find_all('img', decoding=True, nopin=False)
+    image_urls = [image['src'] for image in images]
     st.write(f"Number of images = {len(image_urls)}")
     return image_urls
 
@@ -82,7 +67,7 @@ def sort_filenames(filenames):
 # Parallel downloading using ThreadPoolExecutor
 def download_image_parallel(image_url, chapter, page_number, folder_name):
     try:
-        url = "https://mangaberri.com/" + image_url
+        url =  image_url
         img_size = download_image(url, chapter, page_number)
         os.rename(f'chapter_{chapter}_page_{page_number}.jpg', f'{folder_name}/chapter_{chapter}_page_{page_number}.jpg')
         return img_size
@@ -122,41 +107,23 @@ min_page = st.number_input("minimum", min_value=0)
 max_page = st.number_input("maximum", min_value=0)
 submit_button = st.button("download")
 
-# Fetch all manga links
-response = requests.get("https://mangaberri.com/one-piece-colored-manga")
-soup = BeautifulSoup(response.content, 'html.parser')
-aa = soup.find_all('a', href=True)
-all_decorations = [decoration for decoration in aa if 'colored' in decoration['href']]
-last_chapter = int(all_decorations[1].text)
 
 # Check if max_page exceeds the last chapter available
-if last_chapter < max_page:
-    st.warning(f"Max available chapter is: {last_chapter}")
+if 1129 < max_page:
+    st.warning(f"Max available chapter is: {1129}")
     
 total_size = 0
 pages = range(int(min_page), int(max_page + 1))
 
 # If submit button is clicked
 if submit_button:
-    if anime == "one piece colored":
-        for page in pages:
-            url = urls_extractor("https://mangaberri.com/one-piece-colored-manga", page)
-            image_urls = web_scrape(url)
-            chapter_size = save_images_to_folder(image_urls, f'from_{min_page}_to_{max_page}_{anime}', page)
-            total_size += chapter_size
-    else:
-        for page in pages:
-            if page < 10:
-                url = f"https://readberserk.com/chapter/berserk-chapter-00{page}/"
-            elif page < 100:
-                url = f"https://readberserk.com/chapter/berserk-chapter-0{page}/"
-            else:
-                url = f"https://readberserk.com/chapter/berserk-chapter-{page}/"
-            image_urls = web_scrape(url)
-            chapter_size = save_images_to_folder(image_urls, f'from_{min_page}_to_{max_page}_{anime}', page)
-            total_size += chapter_size
+    for page in pages:
+        url = "https://w12.read-onepiece.net/manga/one-piece-chapter-" + str(page)
+        image_urls = web_scrape(url)
+        chapter_size = save_images_to_folder(image_urls, f'from_{min_page}_to_{max_page}_op', page)
+        total_size += chapter_size
 
-    output_path = f"from_{min_page}_to_{max_page}_{anime}"
+    output_path = f"from_{min_page}_to_{max_page}_op"
     convert_images_to_cbz(output_path)
 
     if os.path.exists(output_path):
